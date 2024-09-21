7dad2d1c0577b3a4f52804e2499d48db2842c9ea
Rutake126 <135197431+Rutake126@users.noreply.github.com>
2024-09-21 23:45:09 +0800

    Update 扫描缺失文件.py

--- 扫描缺失文件.py
+++ 扫描缺失文件.py
@@ -1,10 +1,15 @@
 #在面临大量文件且它们的命名是有规律的，可以通过该脚本查找出哪些文件没有下载
 import os
-folder_path = "file_path"  # 你的文件夹路径如E:\\2025\\hathitrust_images
-total_files = # 总文件数
 
-# 创建所有应有文件名的列表
-expected_files = [f"image_{i:05}.jpg" for i in range(1, total_files + 1)] #根据文件名称进行修改
+# 文件夹路径
+folder_path = "E:\\2025\\downloaded_images"  # 替换为你的文件夹路径
+
+# 起始页和总页数 （根据实际情况更改）
+start_num = 48435315
+total_pages = 885 
+
+# 生成所有应有的文件名
+expected_files = [f"image_{i}.jpg" for i in range(start_num, start_num + total_pages)]
 
 # 获取文件夹中实际存在的文件名
 actual_files = os.listdir(folder_path)
